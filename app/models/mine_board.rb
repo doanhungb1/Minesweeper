78e30d6d69baa27d2840d20f4e6715d274ec4398
doanhungb1 <doanhungb1@gmail.com>
2023-01-12 19:22:00 +0700

    add methods to visualize mine board

--- app/models/mine_board.rb
+++ app/models/mine_board.rb
@@ -3,4 +3,18 @@ class MineBoard < ApplicationRecord
   validates :width, :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :height, :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :mines, presence: true
+  validates :creator_email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+
+  def visualize
+    height.times do |y|
+      width.times do |x|
+        if mine_positions.include?({"mine_x" => x, "mine_y" => y})
+          print('1 ')
+        else
+          print('0 ')
+        end
+      end
+      puts ""
+    end
+  end
 end
