60ef0f06d45def6b064b317af72500ad29c9cf47
doanhungb1 <doanhungb1@gmail.com>
2023-01-12 23:07:48 +0700

    add boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,14 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+var jQuery = require('jquery')
+global.$ = jQuery;
+
+require("bootstrap")
+import "../stylesheets/application";
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
